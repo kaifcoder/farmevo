804e9201dc80581d6869cdf696675190c352e697
Mohd Kaif <kaifmohd2014@gmail.com>
2024-03-14 00:00:38 +0530

    inital comit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-blue-500 text-3xl">App</h1>
+        <Button>Click me</Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
