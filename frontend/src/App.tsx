5c555b8ce768bf771c708a7d3e17213726b29b15
Mohd Kaif <kaifmohd2014@gmail.com>
2024-03-14 14:29:25 +0530

    Add Clerk, Headless UI, and Themes dependencies

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,7 @@
-import { Button } from "./components/ui/button";
-
 function App() {
   return (
     <>
-      <div>
-        <h1 className="text-blue-500 text-3xl">App</h1>
-        <Button>Click me</Button>
-      </div>
+      <header></header>
     </>
   );
 }
